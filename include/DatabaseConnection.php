ea01d43c09e6b8f13284d633003e781c155698b3
Argel Michinel <66443739+ArgelMichinel@users.noreply.github.com>
2023-08-27 20:31:08 -0300

    Update DatabaseConnection.php
    Cambios de datos de acceso a la BD

--- include/DatabaseConnection.php
+++ include/DatabaseConnection.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=u914116908_shipping;charset=utf8', 'u914116908_contacto', '3P*ch&X5g');
+$pdo = new PDO('mysql:host=localhost;dbname=Nombre de BD;charset=utf8', 'NOMBRE DE USUARIO DE BD', 'CLAVE DE ACCESO A BD');
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
